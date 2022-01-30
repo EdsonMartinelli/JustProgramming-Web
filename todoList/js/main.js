35bf81c0d5294a312667efc1e687644eb2084692
EdsonMartinelli <martinelli1997@gmail.com>
2022-01-29 22:20:39 -0300

    update and delete without visual

--- todoList/js/main.js
+++ todoList/js/main.js
@@ -3,7 +3,12 @@
 let addTextField = document.querySelector("#addTextField")
 let addButton = document.querySelector("#addButton")
 let taskArea = document.querySelector("#taskArea")
+
+let allTasks = document.querySelector("#allTasks")
+let remainTasks = document.querySelector("#remainTasks")
+
 let index = 0
+let indexRemain = 0
 let task = {}
 
 addButton.addEventListener("click", () => {
@@ -12,12 +17,25 @@ addButton.addEventListener("click", () => {
             status: "Active",
         }
     task[index++] = newTask
+    allTasks.innerHTML= (index).toString() + " Tasks"
+
+    indexRemain++
+
+    /*Object.keys(task).forEach(key => {
+        if(task[key].status == "Active"){
+            indexRemain++;
+        }
+    })*/
+
+    remainTasks.innerHTML= (indexRemain).toString() + " Remain"
     createItem(newTask)
+    console.log(task)
     
 })
 
 function createItem(newTask){
     let divItem = document.createElement("div")
+    divItem.setAttribute("id", (index - 1).toString())
     divItem.classList.add("item")
 
     let divItemName = document.createElement("div")
@@ -25,10 +43,10 @@ function createItem(newTask){
 
     let checkbox = document.createElement("input")
     checkbox.setAttribute("type", "checkbox");
-    checkbox.setAttribute("id", (index - 1).toString());
+    checkbox.setAttribute("id", "c" + (index - 1).toString());
 
     let label = document.createElement("label")
-    label.setAttribute("for", (index - 1).toString());
+    label.setAttribute("for", "c" + (index - 1).toString());
     label.innerHTML = newTask.task
 
     let divItemActions = document.createElement("div")
@@ -41,14 +59,25 @@ function createItem(newTask){
     spanStatusItem.innerHTML = newTask.status
 
     let deleteButton = document.createElement("button")
+    deleteButton.setAttribute("id", "d" + (index - 1).toString());
     deleteButton.innerHTML = "Delete"
+
+    checkbox.addEventListener("click", e => {
+        let rawId =  e.target.id.split("c")
+        task[rawId[1]].status = "Inactive"
+        e.target.disabled = true
+    })
+
+    deleteButton.addEventListener("click", e => {
+        let rawId =  e.target.id.split("d")
+        delete task[rawId[1]]
+    })
+
 
     divItemName.appendChild(checkbox)
     divItemName.appendChild(label)
 
-    divStatus.appendChild(spanStatusItem)
-
-    divItemActions.appendChild(divStatus)
+    divItemActions.appendChild(divStatus).appendChild(spanStatusItem)
     divItemActions.appendChild(deleteButton)
 
     divItem.appendChild(divItemName)
